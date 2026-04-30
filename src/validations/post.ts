1090ecaf02a10a4f084c56df4f29f895fc24d7d2
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-04-30 21:35:34 +0300

    feat: add image in post type to be required and update zod validation of add new post

--- src/validations/post.ts
+++ src/validations/post.ts
@@ -3,6 +3,20 @@ import { z } from "zod";
 export const addPostSchema = z.object({
   title: z.string().trim().min(1, { message: "Post Title Is Required!" }),
   body: z.string().trim().min(1, { message: "Post Body Is Required!" }),
+  image: z.custom(
+    (value) => {
+      if (!value || typeof value !== "object" || !(value instanceof File)) {
+        return false;
+      }
+
+      const validMimiTypes = ["image/jpeg", "image/png", "image/gif"];
+
+      return validMimiTypes.includes(value.type);
+    },
+    {
+      message: "Post Image Is Required",
+    },
+  ),
 });
 
 export const updatePostSchema = z.object({
