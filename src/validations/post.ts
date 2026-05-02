03b67f685a7ba2236b1ac10e9e5137449564e193
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-02 17:42:40 +0300

    feat: add image validation in update-post-schema

--- src/validations/post.ts
+++ src/validations/post.ts
@@ -27,4 +27,23 @@ export const addPostSchema = z.object({
 export const updatePostSchema = z.object({
   title: z.string().trim().min(1, { message: "Add Post Title To Update!" }),
   body: z.string().trim().min(1, { message: "Add Post Body To Update!" }),
+  image: z.custom(
+    (value) => {
+      if (!value || typeof value !== "object" || !(value instanceof File)) {
+        return false;
+      }
+
+      const validMimiTypes = [
+        "image/jpeg",
+        "image/png",
+        "image/gif",
+        "image/jpg",
+      ];
+
+      return validMimiTypes.includes(value.type);
+    },
+    {
+      message: "Post Image Is Required To Update Post",
+    },
+  ),
 });
