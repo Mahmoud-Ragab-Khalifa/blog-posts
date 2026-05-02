670be177347a623710e32d48397c026484fb2855
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-05-02 19:19:22 +0300

    refactor: set image-validation as reusable function accept required flag to use it in create and update post forms

--- src/validations/post.ts
+++ src/validations/post.ts
@@ -1,49 +1,39 @@
 import { z } from "zod";
 
+const imageValidation = (isRequired: boolean) => {
+  return !isRequired
+    ? z.custom((value) => value instanceof File)
+    : z.custom(
+        (value) => {
+          if (typeof value !== "object" || !value || !(value instanceof File)) {
+            return false;
+          }
+
+          const validMimeTypes = [
+            "image/jpeg",
+            "image/jpg",
+            "image/png",
+            "image/gif",
+            "image/webp",
+          ];
+
+          return validMimeTypes.includes(value.type);
+        },
+
+        {
+          message: "Post Image Is Required!",
+        },
+      );
+};
+
 export const addPostSchema = z.object({
   title: z.string().trim().min(1, { message: "Post Title Is Required!" }),
   body: z.string().trim().min(1, { message: "Post Body Is Required!" }),
-  image: z.custom(
-    (value) => {
-      if (!value || typeof value !== "object" || !(value instanceof File)) {
-        return false;
-      }
-
-      const validMimiTypes = [
-        "image/jpeg",
-        "image/png",
-        "image/gif",
-        "image/jpg",
-      ];
-
-      return validMimiTypes.includes(value.type);
-    },
-    {
-      message: "Post Image Is Required",
-    },
-  ),
+  image: imageValidation(true),
 });
 
 export const updatePostSchema = z.object({
   title: z.string().trim().min(1, { message: "Add Post Title To Update!" }),
   body: z.string().trim().min(1, { message: "Add Post Body To Update!" }),
-  image: z.custom(
-    (value) => {
-      if (!value || typeof value !== "object" || !(value instanceof File)) {
-        return false;
-      }
-
-      const validMimiTypes = [
-        "image/jpeg",
-        "image/png",
-        "image/gif",
-        "image/jpg",
-      ];
-
-      return validMimiTypes.includes(value.type) || value instanceof File;
-    },
-    {
-      message: "Post Image Is Required To Update Post",
-    },
-  ),
+  image: imageValidation(false),
 });
